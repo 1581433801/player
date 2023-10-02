268460be4f87c437008159927ebd62b208265161
xingqi <xingqi@qq.com>
2023-10-02 21:25:36 +0800

    xgplayer引入测试

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Player from 'xgplayer'
+import { onMounted } from 'vue'
+import 'xgplayer/dist/index.min.css'
+onMounted(() => {
+  let player = new Player({
+    id: 'mse',
+    url: 'https://sample-videos.com/video123/mp4/480/big_buck_bunny_480p_1mb.mp4',
+    videoInit: true,
+    closeInactive: false,
+    autoplay: true,
+    loop: true,
+    fitVideoSize: 'fixWidth'
+  })
+})
+</script>
 
 <template>
-  <div class="home-page">home</div>
+  <div id="mse"></div>
 </template>
 
 <style lang="scss" scoped></style>
