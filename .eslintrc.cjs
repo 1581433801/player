c107cf4fca6b75208d978d6807e1559adf315041
xingqi <xingqi@qq.com>
2023-10-02 16:09:48 +0800

    eslint和router配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,26 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        // 允许index文件名
+        ignores: ['index']
+      }
+    ],
+    // 允许解构props
+    'vue/no-setup-props-destructure': ['off']
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Home/index.vue') // 路由懒加载
+    }
+  ]
 })
 
 export default router
